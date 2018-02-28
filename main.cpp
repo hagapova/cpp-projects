4b679941057e17c9f70cee4212c1f1897661e9f5
hagapova <36931014+hagapova@users.noreply.github.com>
2018-02-28 21:14:15 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+class Figure {
+public:
+    virtual double Area() const = 0;
+};
+
+class Circle : public Figure {
+private:
+    double radius;
+public:
+    Circle(double r) {
+        radius = r;
+    }
+    double Area() const override {
+        return (this->radius)*(this->radius)*3.1415926;
+    }
+};
+
+class Square : public Figure {
+private:
+    double side;
+public:
+    Square(double r) {
+        side = r;
+    }
+    double Area() const override {
+        return (this->side)*(this->side);
+    }
+};
+
+int main() {
+    std::vector<Figure*> figures;
+
+    std::string type = "Circle";
+    double val = 1.0;
+
+
+    if (type == "Circle")
+        figures.push_back(new Circle(val));
+    else if (type == "Square")
+        figures.push_back(new Square(val));
+
+    for (Figure * f : figures)
+        std::cout << f->Area() << "\n";
+
+    for (Figure * f : figures)
+        delete f;
+
+}
+
